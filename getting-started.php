ea695a6c3e4d8dd9524ab936b78b8ac7be417893
foresitegroup <mark@foresitegrp.com>
2017-03-02 17:14:42 -0600

    Update copy

--- getting-started.php
+++ getting-started.php
@@ -17,23 +17,17 @@ include "header.php";
 <hr class="full mb">
 
 <article>
-  <h5 class="redtext">PURCHASING SUPPORT? PLEASE READ</h5>
-  With the new release of Investment Account Manager v3 for Individuals, technical support renewals are no longer available for your older version of Investment Account Manager v2.  To renew support, you need to first download and install a new demo of Investment Account Manager v3, then once installed, start the demo, and while in demo mode, use the file menu | <a href="how-to-converting-files-from-iam2-to-new-iam3.php">convert IAM2 Individual to IAM3 individual</a>.  Once your data has been converted, a new user ID will be created that can be used for renewing support for Investment Account Manager 3 for Individuals.<br>
+  If you have any questions while using the free 30 day demo version of Investment Account Manager v3 Individual, please call our technical support team at 800-247-6354.<br>
   <br>
 
-  If you have any questions, please call Investment Account Manager technical support at 800-247-6354.<br>
-  <br>
-  <br>
-
-  <strong>So, let's get started.</strong><br>
-  <br>
-
-  <strong>If you haven't installed the trial version of IAM do so now. IAM will run on any Windows based system. IAM is not intended to run on networks.</strong><br>
-  <span class="redtext">*</span> Note: IAM can run on Apple operating systems, emulating Windows.<br>
+  <a href="pdf/Getting_Started.pdf">Print This Guide</a><br>
   <br>
   <br>
 
-  <h5>INSTALLING IAM3</h5>
+  <h5>GETTING STARTED</h5>
+
+  Installing the free 30 day demo version of Investment Account Manager 3 Individual:<br>
+  <br>
 
   <ol class="getting-started">
     <li class="long-string"><strong><a href="download-iam.php">Download the 30 day trial version</a> from our website &mdash; www.investmentaccountmanager.com.  The file name is IAM3_Setup.exe.</strong></li>
@@ -41,60 +35,69 @@ include "header.php";
     <li>
       <strong>Install the trial.</strong>
       <ul>
-        <li>Carefully read the License Agreement &mdash; IAM3 is not intended for commercial use.  If you are a commercial user, please see our Pro Version.</li>
-        <li>Additionally, we allow for two installations of the software per user.  This is to assure that you have access to IAM.</li>
+        <li>Carefully read the License Agreement &mdash; IAM3 Individual is not intended for commercial use. <em>If you are a commercial/professional user, please see our Pro Version.</em></li>
+        <li>Additionally, we allow for two installations of the software per user. This is to assure that you have access to IAM.</li>
         <li>We recommend using the default settings while installing IAM.</li>
+        <li>IAMv3 will run on any Windows based system. IAMv3 is not intended to run on
+networks. IAM can run on Apple operating systems, emulating Windows.</li>
       </ul>
     </li>
 
     <li>
-      <strong>Launch Investment Account Manager</strong>
+      <strong>Launch Investment Account Manager v3 Individual</strong>
       <ul>
-        <li>If asked to download previous month-end prices, click on yes.</li>
         <li>Complete the Welcome Screen by entering the name of the person to whom the software will be registered.</li>
         <li>Note that the software demo has a 30 day free trial period.</li>
+      </ul>
+    </li>
+
+    <li>
+      <strong>The IAMv3 Home Page</strong>
+      <ul>
+        <li>You should now be looking at IAM's Home Page.  Note that the window includes drop-down menus, a tool bar, and tabs. The home page allow for easy navigation through the various windows and reports found in IAMv3.</li>
         <li>
-          Since we frequently update IAM, please click on the <strong>Check for Updates</strong> button found in the tool bar.<br>
-          If a newer version of IAM is found, please download it.
+          Select the Help Menu | Contents Choice.
           <ul>
-            <li>After the download is complete, exit IAM.</li>
-            <li>Click on the Windows Start Menu.</li>
-            <li>Click on All Apps (All Programs).</li>
-            <li>Find and click on Investment Account Manager 3 program group.</li>
-            <li>Click on Update IAM and install the update.</li>
-            <li>Start the demo again, now updated, by double-clicking on the IAM3 desktop Icon.</li>
+            <li>In the left-hand help window pane, select the book titled Chapter 1
+Introduction.</li>
+            <li>Select the book titled Home Page Intro.</li>
+            <li>We suggest reviewing the various topics describing the IAMv3 home page
+features and tab views.</li>
+            <li>You can view a tutorial video of the IAM3 Individual home page on the <a href="video-tutorials.php">tutorial page</a>.
           </ul>
         </li>
       </ul>
     </li>
 
     <li>
-      <strong>Back to the updated program... IAM's Home Page</strong>
+      <strong>Reviewing IAM v3 Sample Reports</strong><br>
       <ul>
         <li>
-          You should now be looking at IAM's Home Page.  Note that the window includes drop-down menus, a tool bar, and tabs. The home page allow for easy navigation through the various windows and reports found in IAM.
+          Select the Help Menu | Contents Choice
           <ul>
-            <li>Drop-down menus provide access to main features.</li>
-            <li>Tool-bar provides quick access to commonly used features.</li>
-            <li>Tabs provide different views of various views of the open portfolio</li>
+            <li>In the left-hand help window pane, select the book titled Chapter 1 Introduction</li>
+            <li>Select the book titled Reports and Graphs Intro.</li>
+            <li>Select the help topic IAM Reports and Graphs.</li>
+            <li>We suggest using the Print icon to print this help topic and then to follow this as a guide to review reports using the IAM Demo Portfolio 3 for tutorial purposes.</li>
+            <li>Once you've completed reviewing the help topic guide, select other reports as desired (note that you can combine individual portfolios onto a single report, for cumulative reporting. Very helpful for the "big picture" view).</li>
           </ul>
         </li>
       </ul>
     </li>
 
     <li>
-      <strong>Review IAM's sample data included in the demo</strong><br>
+      <strong>Reviewing Other IAM v3 Sample Data</strong><br>
       <ul>
         <li>
           First, update the prices of the sample securities (IAM includes access to free 20 minute delayed pricing from Yahoo! Finance and others).
           <ul>
             <li>Click on Update Prices button found in Tool Bar.</li>
-            <li>Notice IAM will offer a chance to review price --alerts (something you will learn to setup later to notify you of important price changes).</li>
-            <li>After updating, use Exit button to return to the Home Page.</li>
+            <li>If any sample price alerts have been triggered, you'll be prompted to review these.</li>
+            <li>Once prices have been updated, use the OK button to return to the Home Page.</li>
             <li>
-              The left panel of the Home Page provides access to the different portfolios being monitored w/I IAM
+              The left panel of the Home Page provides access to the different sample portfolios provided with the IAMv3 demo version.
               <ul>
-                <li>Click on portfolio to change to that portfolio, i.e., open that portfolio.</li>
+                <li>Click on another portfolio to change to that portfolio, i.e., open that portfolio for the IAMv3 home page display.</li>
               </ul>
             </li>
           </ul>
@@ -103,105 +106,65 @@ include "header.php";
     </li>
 
     <li>
-      <strong>Experiment using the different Home Page tabs by clicking on a tab. Each tab is designed to quickly provide you with specific information.</strong>
+      <strong>Entering Sample Data</strong><br>
+      IAM allows users to either download their data from financial institutions, to manually enter their data using ledgers and windows, or to import their data from third party applications. A complete description of these methods is provided in the online help, as well as the online manual.<br>
+      <br>
+
+      For this quick setup guide, we'll take a look at manual entry using the IAMv3 Activity Ledger. You can view a tutorial video of the IAM3 Activity Ledger on the <a href="video-tutorials.php">tutorial page</a>.<br>
+      <br>
+
       <ul>
         <li>
-          Many of the tabs can be customized for reporting period.<br>
-          * Note: use Help buttons for a detailed explanation for the open window (IAM provides a detailed help system via the Help drop-down menu also).
+          Select the Transactions Menu.
+          <ul>
+            <li>Select Add, Edit, Delete.</li>
+            <li>The transactions ledger provides users with a method to manually enter their data in ledger format, left-to-right, using drop downs to select transaction types, etc. Click on the next empty line in the ledger to try entering the following.</li>
+            <li>Verify the radio button above the column labeled Security is selected to show All Securities.</li>
+            <li>Click your mouse in the column labeled Security, and use the drop down to select a security provided with the IAMv3 sample demo data (securities are added as you build your portfolios).</li>
+            <li>
+              Select Caterpillar Inc.
+              <ul>
+                <li>Under the Activity column select Purchase.</li>
+                <li>Under the Quantity column enter 100.</li>
+                <li>In the column labeled Unit Price, enter 97.50.</li>
+                <li>In the column Comm (commissions and fees), enter 10.00</li>
+                <li>Notice the Dollar Amount is automatically populated for the cost of this purchase activity.</li>
+                <li>Prior to selecting the Save button, notice in the lower left-hand corner a withdrawal is selected for the money market offset for this purchase transaction. (This is known as sweeping or cash pooling). In the lower right portion of the activity ledger, users can define the offset settlement date to use trade date + 3 or settlement date = trade date.</li>
+                <li>Select the Save button.</li>
+                <li>Select the Exit button to close the activity ledger.</li>
+              </ul>
+            </li>
+          </ul>
         </li>
+        <li>The other method to enter transactions we call our "Classic View" (Transactions Menu | Add Edit Delete | Classic View). This method provides sequential data entry screens to record your transactions. You can try this enrty method as well, referring to the help topics as you go along.</li>
       </ul>
     </li>
 
     <li>
-      <strong>Review IAM's reporting capabilities.</strong>
+      <strong>Creating Your Portfolio</strong>
+      <ul>
+        <li>IAMv3 Individual allows for an unlimited number of portfolios, to provide a unified tool for multiple portfolio management.</li>
+        <li>Entering your portfolios into IAMv3 is easy and flexible using the Portfolio Setup Wizard found on the toolbar. It provides several methods to start tracking your accounts.</li>
+        <li>You can view three video tutorials on using the Portfolio Setup a tutorial to create your portfolios on the <a href="video-tutorials.php">tutorial page</a>.</li>
+      </ul>
+    </li>
+
+    <li>
+      <strong>Getting Help</strong>
       <ul>
         <li>
-          Select the Reports drop-down Menu.
+          Our Technical Support Team offers free unlimited support while you use the IAMv3 30 day demo version.
           <ul>
-            <li>
-              Select Portfolio reports, then Appraisal Report.
-              <ul>
-                <li>Review the standard report set-up window.  Note that you can combine individual portfolios onto a single report, for cumulative reporting. Very helpful for the "big picture" view.</li>
-                <li>Click on the Report button.</li>
-              </ul>
-            </li>
-            <li>Select other reports as desired.</li>
+            <li class="long-string">Email us at <strong><?php email("techsupport@investmentaccountmanager.com"); ?></strong></li>
+            <li>Call us: <strong>262-241-3990</strong></li>
+            <li class="long-string">Visit our website at: <strong>www.investmentaccountmanager.com</strong></li>
           </ul>
         </li>
       </ul>
     </li>
+  </ol>
 
-    <li>
-      <strong>Entering data into IAM.</strong> IAM allows users to either download their data from financial institutions, to manually enter their data using ledgers and windows, or to import their data from third party applications. A complete description of these methods is provided in the online help, as well as the online manual.
-      <ul>
-        <li>
-          For this quick setup guide, we'll take a look at manual entry.
-          <ul>
-            <li>
-              Select Transactions Menu.
-              <ol type="a">
-                <li><span>
-                  Select Add, Edit, Delete.
-                  <ul>
-                    <li>The transactions ledger provides users with a method to manually enter their data in ledger format, left-to-right, using drop downs to select transaction types, etc. Click on the nest empty line in the ledger to try entering the following.</li>
-                    <li>Under security use drop down to select a security previously entered in the demo (securities are added as you build your portfolios). Select Caterpillar Inc.</li>
-                  </ul></span>
-                </li>
-                <li><span>Under the Activity column select purchase.</li></span>
-                <li><span>Under the Quantity column enter 100.</li></span>
-                <li><span>Under the Dollar Amount column, enter the 6650.00 (net amount of purchase) then use the enter key and click on save.</li></span>
-                <li><span>The demo uses a money market offset &mdash; notice a withdrawal is automatically made for the amount of the trade (This is known as sweeping or cash pooling).</li></span>
-              <li><span>Click the OK button found at the bottom of the ledger to save and close.</span></li>
-            </ol>
-          </li>
-        </ul>
-      </li>
-      <li>
-        The other method to enter transactions we call our "Classic View" (we have developed portfolio management software since 1985). This method provides sequential data entry screens to record your transactions. Under this method, we first review the Activity Ledger to check what has been previously entered.
-        <ul>
-          <li>Click on the Enter New button.</li>
-          <li>Enter the date.</li>
-          <li>Select the security from the list of previously entered securities, or Add New in the drop-down list.</li>
-          <li>Scroll down to select Caterpillar.</li>
-          <li>Click on Purchase button.</li>
-          <li>Enter 100 as the quantity.</li>
-          <li>6850 as the net amount.</li>
-          <li>Click on the Save button.</li>
-          <li>Click on the red-x in the upper-right corner of the Activity Ledger window.</li>
-        </ul>
-      </li>
-    </ul>
-  </li>
-
-  <li>
-    Entering your portfolios into IAM is easy and flexible using the Portfolio Setup Wizard found in the toolbar.  It provides several methods to start tracking our accounts. <strong>Click on the Portfolio Wizard button</strong>.
-    <ul>
-      <li>Enter a portfolio name (portfolio is synonymous with investment account).  Example might be Peter's 401K.</li>
-      <li>
-        Select one of the methods to proceed.  We suggest that you use the Learn More links to help you decide. Most new users start with their current portfolio balances. All that is needed is a summary of what you own, when you bought it, and what you paid.<br>
-        NOTE: it's worth it to accurately enter the lot-by-lot data of your current holdings Since IAM provides detailed tax lot accounting as well as detailed performance measurements.
-        <ul>
-          <li>IAM allows for an unlimited number of portfolios</li>
-          <li>IAM allows for easy deletion of portfolios</li>
-        </ul>
-      </li>
-    </ul>
-  </li>
-
-  <li>
-    <strong>Getting help</strong>
-    <ul>
-      <li class="long-string">Email us at <strong><?php email("techsupport@investmentaccountmanager.com"); ?></strong></li>
-      <li>Call us: <strong>262-241-3990</strong></li>
-      <li class="long-string">Visit our website at: <strong>www.investmentaccountmanager.com</strong></li>
-    </ul>
-  </li>
-</ol>
-
-<strong>Thank you for using Investment Account Manager.</strong><br>
-<br>
-
-<a href="pdf/Getting_Started.pdf">Print This Guide</a>
+  Thank you for considering Investment Account Manager v3 Individual for your investing needs. Whether you're a seasoned investor or a novice, you'll find Investment Account Manager 3 Individual offers a comprehensive set of features - yet is an easy to learn and use portfolio management software tool.
 </article>
 
 <div class="darkgray whitetext footer bt">
--- iam3-individual.php
+++ iam3-individual.php
@@ -17,28 +17,10 @@ include "header.php";
 <hr class="full mb">
 
 <article>
-  We're confident you'll find the new IAM3 Individual version our best release yet, filled with new features and improvements, including suggestions from users such as yourself. A few of the more notable improvements include:<br>
+  We're confident you'll find the new IAMv3 Individual version our best release yet, filled with new features and improvements, including suggestions from users such as yourself.<br>
   <br>
 
-  <ul class="redcircles" style="font-weight: bold;">
-    <li>Re-designed IAM home page<br><br></li>
-    <li>New transaction activity ledger<br><br></li>
-    <li>Expanded allocation by asset types<br><br></li>
-    <li>Performance storing and comparison reporting<br><br></li>
-    <li>Portfolio grouping for reporting<br><br></li>
-    <li>Expanded list of supported financial institutions<br><br></li>
-    <li>Updated interface and look<br><br></li>
-    <li>New more comprehensive help system<br><br></li>
-    <li>Plus much more...</li>
-  </ul>
-  <br>
-
-  All users of Investment Account Manager 2 Individual version, with active support policies, qualify for a free upgrade to the new IAM3 Individual version! Simply download the new trial version of Investment Account Manager 3 individual. Once installed, and after exploring IAM3 individual, you can easily convert your existing IAM2 Individual version data. <em style="color: #ED243B;">Please note: support renewals for IAM2 are no longer available.</em><br>
-  <br>
-  <br>
-
-  <strong>Installing Investment Account Manager 3</strong><br>
-  Follow these steps for downloading a demo of IAM3. Note: when you are ready to convert your current IAMv2 data, as long as your current IAM2 is up-to-date with active support, the IAM3 demo will convert to a full version!<br>
+  Follow these steps for downloading a demo of IAMv3.  When you convert your current IAMv2 data, as long as your current IAMv2 is up-to-date with active support, the IAM3 demo will convert to a full version.  If your IAMv2 support has expired, once you have installed and converted your existing IAMv2 data into IAMv3, you can order support renewal codes for IAMv3 to convert the demo to a full version.  You do not need to order a new software activation key.<br>
   <br>
 
   <ol>
@@ -46,58 +28,30 @@ include "header.php";
     <li>Once downloaded, install the new IAMv3 demo, using the default locations as provided during the installation process.<br><br></li>
     <li>Once installed, start the IAMv3 demo.<br><br></li>
     <li>
-      Spend time using the provided sample IAM3 data to:<br><br>
-      <ul class="redcircles">
-        <li>Review new features, including the new home page layout<br><br></li>
-        <li>Make sample entries in the new ledger system using the provided sample portfolios.<br><br></li>
-        <li>Test as you like to get a good feel for IAMv3.<br><br></li>
-        <li>When you are ready, close the IAMv3 demo, and proceed with the remaining steps below....<br><br></li>
-      </ul>
-    </li>
-  </ol>
-  <br>
-
-  <strong>Converting IAM2 data to IAM3</strong><br>
-  Prior to converting your IAM2 data to IAM3, we recommend making a backup of your current IAM2 data.<br>
-  <br>
-
-  <ol>
-    <li>Start your copy of IAM2, and use the file menu | backup data choice | backing up your data to a removable drive.<br><br></li>
-    <li>Close IAM2.</li>
-  </ol>
-  <br>
-
-  When you are ready, you can then convert your existing IAM2 data to IAM3. Note: your IAM2 data will remain unchanged and intact for continued use in IAM2 to shadow entry activity while using IAM3.<br>
-  <br>
-
-  <ol>
-    <li>Start the IAM3 demo.<br><br></li>
-    <li>
       Select the file menu | convert IAM2 Individual into IAM3 Individual.<br>
       <br>
 
       <img src="images/iam3-individual1.jpg" alt=""><br><br>
     </li>
-    <li>Select the OK button when prompted to continue with the conversion of your IAM2 individual data into IAM3 Individual version.<br><br></li>
+    <li>Select the OK button when prompted to continue with the conversion of your IAMv2 individual data into IAMv3 Individual version.<br><br></li>
     <li>
-      If you used the original default location for your IAM2 installation, your portfolios should be listed for conversion. Select the '<strong>Convert Now</strong>' button.<br>
+      If you used the original default location for your IAMv2 installation, your portfolios should be listed for conversion. Select the '<strong>Convert Now</strong>' button.<br>
       <br>
 
       <img src="images/iam3-individual2.jpg" alt=""><br>
       <br>
 
-      <ul class="redcircles"><li>Note:  If you did not use the original default location for IAM2, use the 'Directory' button to locate the installation directory for your current copy of IAM2, and then select the \Data folder. Your portfolios should be listed for conversion. Select the '<strong>Convert Now</strong>' button.</li></ul>
+      <ul class="redcircles"><li>Note:  If you did not use the original default location for IAMv2, use the 'Directory' button to locate the installation directory for your current copy of IAMv2, and then select the \Data folder. Your portfolios should be listed for conversion. Select the '<strong>Convert Now</strong>' button.</li></ul>
       <br>
     </li>
-    <li>All existing portfolio data will be migrated to IAM3, and IAM3 will be activated to a full version, provided your IAM2 support is up-to-date and active.<br><br></li>
-    <!-- <li>We suggest you use IAM3 for a couple of months in shadow to your regular use of IAM2, verifying activity, updating, and reporting.</li> -->
+    <li>All existing portfolio data will be migrated to IAMv3, and IAMv3 will be activated to a full version, provided your IAMv2 support is up-to-date and active.  If your IAMv2 support has expired, once you have installed and converted your existing IAMv2 data into IAMv3, you can order support renewal codes for IAMv3 to convert the demo to a full version.<br><br></li>
   </ol>
   <br>
 
-  <em style="color: #ED243B;">If your IAM2 support has expired, once you have installed and converted your existing IAM2 data into IAM3, you can order support renewal codes for IAM3 to convert the demo to a full version.</em><br>
+  Thank you for your continued use of Investment Account Manager. We appreciate you as a customer, and are confident you'll find IAMv3 Individual version even more helpful in successfully managing your investment portfolio(s).<br>
   <br>
 
-  Thank you for your continued use of Investment Account Manager. We appreciate you as a customer, and are confident you'll find IAM3 Individual version even more helpful in successfully managing your investment portfolio(s).<br>
+  If you have any questions, please contact our technical support team at: 262-241-3990. Email: <?php email("techsupport@investmentaccountmanager.com"); ?>.<br>
   <br>
 
   Matt and Peter Willms<br>
