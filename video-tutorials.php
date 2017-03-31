6df091e0f32f24a6570378238bee940fc9788ef9
foresitegroup <mark@foresitegrp.com>
2017-03-31 09:25:57 -0500

    Add anchor links; force 720 quality for videos

--- video-tutorials.php
+++ video-tutorials.php
@@ -30,9 +30,9 @@ include "header.php";
 <div class="videos">
   <article>
     <section>
-      <h2>General Features</h2>
+      <h2 id="general">General Features</h2>
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=gHlQY4vjPrw&rel=0&showinfo=0" class="swipebox-video" rel="gHlQY4vjPrw"></a>
+        <a href="https://www.youtube.com/watch?v=gHlQY4vjPrw&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="gHlQY4vjPrw"></a>
 
         <div class="video-box-text">
           <h4>Getting Started</h4>
@@ -41,7 +41,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=Zr4_qbYTOYQ&rel=0&showinfo=0" class="swipebox-video" rel="Zr4_qbYTOYQ"></a>
+        <a href="https://www.youtube.com/watch?v=Zr4_qbYTOYQ&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="Zr4_qbYTOYQ"></a>
 
         <div class="video-box-text">
           <h4>Home Page</h4>
@@ -50,7 +50,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=qy_SDpFtQJg&rel=0&showinfo=0" class="swipebox-video" rel="qy_SDpFtQJg"></a>
+        <a href="https://www.youtube.com/watch?v=qy_SDpFtQJg&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="qy_SDpFtQJg"></a>
 
         <div class="video-box-text">
           <h4>Reports</h4>
@@ -59,7 +59,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=WOqUKI3hpio&rel=0&showinfo=0" class="swipebox-video" rel="WOqUKI3hpio"></a>
+        <a href="https://www.youtube.com/watch?v=WOqUKI3hpio&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="WOqUKI3hpio"></a>
 
         <div class="video-box-text">
           <h4>Activity Ledger</h4>
@@ -68,7 +68,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=rtFFDFtyN7Y&rel=0&showinfo=0" class="swipebox-video" rel="rtFFDFtyN7Y"></a>
+        <a href="https://www.youtube.com/watch?v=rtFFDFtyN7Y&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="rtFFDFtyN7Y"></a>
 
         <div class="video-box-text">
           <h4>Current Asset Library</h4>
@@ -77,7 +77,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=oJpE7py3itw&rel=0&showinfo=0" class="swipebox-video" rel="oJpE7py3itw"></a>
+        <a href="https://www.youtube.com/watch?v=oJpE7py3itw&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="oJpE7py3itw"></a>
 
         <div class="video-box-text">
           <h4>Historic Price Library</h4>
@@ -88,9 +88,9 @@ include "header.php";
 
     
     <section>
-    <h2>Portfolio Setup Wizard Features</h2>
+    <h2 id="portfolio-setup-wizard">Portfolio Setup Wizard Features</h2>
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=haAsNSbTGJI&rel=0&showinfo=0" class="swipebox-video" rel="haAsNSbTGJI"></a>
+        <a href="https://www.youtube.com/watch?v=haAsNSbTGJI&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="haAsNSbTGJI"></a>
 
         <div class="video-box-text">
           <h4>Manually Enter Current Holdings</h4>
@@ -99,7 +99,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=a0Aq__qJHnA&rel=0&showinfo=0" class="swipebox-video" rel="a0Aq__qJHnA"></a>
+        <a href="https://www.youtube.com/watch?v=a0Aq__qJHnA&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="a0Aq__qJHnA"></a>
 
         <div class="video-box-text">
           <h4>Download Current Holdings From Your Financial Institution</h4>
@@ -108,7 +108,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=DT9Vk3rmgc0&rel=0&showinfo=0" class="swipebox-video" rel="DT9Vk3rmgc0"></a>
+        <a href="https://www.youtube.com/watch?v=DT9Vk3rmgc0&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="DT9Vk3rmgc0"></a>
 
         <div class="video-box-text">
           <h4>Import Current Holdings From A Microsoft XLS/CSV File</h4>
@@ -119,9 +119,40 @@ include "header.php";
 
 
     <section>
-      <h2>QuoteMedia Data Feed Features</h2>
+      <h2 id="historical-accurate-portfolio">Historical Accurate Portfolio</h2>
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=TMkE_YTQaBk&rel=0&showinfo=0" class="swipebox-video" rel="TMkE_YTQaBk"></a>
+        <a href="https://www.youtube.com/watch?v=aDEz7hD0k-w&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="aDEz7hD0k-w"></a>
+
+        <div class="video-box-text">
+          <h4>Historical Accurate Portfolio Part 1</h4>
+          8:10
+        </div>
+      </div>
+
+      <div class="video-box">
+        <a href="https://www.youtube.com/watch?v=vB0orNAvUiw&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="vB0orNAvUiw"></a>
+
+        <div class="video-box-text">
+          <h4>Historical Accurate Portfolio Part 2</h4>
+          8:07
+        </div>
+      </div>
+
+      <div class="video-box">
+        <a href="https://www.youtube.com/watch?v=Lfqb0TqQzNg&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="Lfqb0TqQzNg"></a>
+
+        <div class="video-box-text">
+          <h4>Historical Accurate Portfolio Part 3</h4>
+          7:53
+        </div>
+      </div>
+    </section>
+
+
+    <section>
+      <h2 id="quotemedia">QuoteMedia Data Feed Features</h2>
+      <div class="video-box">
+        <a href="https://www.youtube.com/watch?v=TMkE_YTQaBk&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="TMkE_YTQaBk"></a>
 
         <div class="video-box-text">
           <h4>QuoteMedia Data Feed: Updating New Securities</h4>
@@ -130,7 +161,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=V7eE6-QEDIU&rel=0&showinfo=0" class="swipebox-video" rel="V7eE6-QEDIU"></a>
+        <a href="https://www.youtube.com/watch?v=V7eE6-QEDIU&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="V7eE6-QEDIU"></a>
 
         <div class="video-box-text">
           <h4>QuoteMedia Data Feed: Updating Existing Securities</h4>
@@ -141,9 +172,9 @@ include "header.php";
 
     
     <section>
-      <h2>Portfolio Allocation Features</h2>
+      <h2 id="portfolio-allocation">Portfolio Allocation Features</h2>
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=EzL-01ew4Kc&rel=0&showinfo=0" class="swipebox-video" rel="EzL-01ew4Kc"></a>
+        <a href="https://www.youtube.com/watch?v=EzL-01ew4Kc&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="EzL-01ew4Kc"></a>
 
         <div class="video-box-text">
           <h4>Asset Allocation (1): Setting Portfolio Allocation Targets</h4>
@@ -152,7 +183,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=uj5-r-su6eM&rel=0&showinfo=0" class="swipebox-video" rel="uj5-r-su6eM"></a>
+        <a href="https://www.youtube.com/watch?v=uj5-r-su6eM&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="uj5-r-su6eM"></a>
 
         <div class="video-box-text">
           <h4>Asset Allocation (2): Current Allocations &amp; Rebalancing</h4>
@@ -163,9 +194,9 @@ include "header.php";
 
     
     <section>
-      <h2>Portfolio Performance Features</h2>
+      <h2 id="portfolio-performance">Portfolio Performance Features</h2>
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=gLGX-4FpGI0&rel=0&showinfo=0" class="swipebox-video" rel="gLGX-4FpGI0"></a>
+        <a href="https://www.youtube.com/watch?v=gLGX-4FpGI0&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="gLGX-4FpGI0"></a>
 
         <div class="video-box-text">
           <h4>Performance (1): Creating Historical Price Files</h4>
@@ -174,7 +205,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=RcAObzZzdXQ&rel=0&showinfo=0" class="swipebox-video" rel="RcAObzZzdXQ"></a>
+        <a href="https://www.youtube.com/watch?v=RcAObzZzdXQ&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="RcAObzZzdXQ"></a>
 
         <div class="video-box-text">
           <h4>Performance (2): Portfolio Performance Report</h4>
@@ -183,7 +214,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=sgHT2GfA7mg&rel=0&showinfo=0" class="swipebox-video" rel="sgHT2GfA7mg"></a>
+        <a href="https://www.youtube.com/watch?v=sgHT2GfA7mg&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="sgHT2GfA7mg"></a>
 
         <div class="video-box-text">
           <h4>Performance (3): Deriving and Storing Performance Returns</h4>
@@ -192,7 +223,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=v2mhEwlyaSg&rel=0&showinfo=0" class="swipebox-video" rel="v2mhEwlyaSg"></a>
+        <a href="https://www.youtube.com/watch?v=v2mhEwlyaSg&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="v2mhEwlyaSg"></a>
 
         <div class="video-box-text">
           <h4>Performance (4): Defining Benchmark Indices</h4>
@@ -201,7 +232,7 @@ include "header.php";
       </div>
 
       <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=-QPN-jU6k2I&rel=0&showinfo=0" class="swipebox-video" rel="-QPN-jU6k2I"></a>
+        <a href="https://www.youtube.com/watch?v=-QPN-jU6k2I&rel=0&showinfo=0&vq=hd720" class="swipebox-video" rel="-QPN-jU6k2I"></a>
 
         <div class="video-box-text">
           <h4>Performance (5): Portfolio Performance Comparisons</h4>
@@ -209,37 +240,6 @@ include "header.php";
         </div>
       </div>
     <section>
-
-
-    <section>
-      <h2>Historical Accurate Portfolio</h2>
-      <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=aDEz7hD0k-w&rel=0&showinfo=0" class="swipebox-video" rel="aDEz7hD0k-w"></a>
-
-        <div class="video-box-text">
-          <h4>Historical Accurate Portfolio Part 1</h4>
-          8:10
-        </div>
-      </div>
-
-      <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=vB0orNAvUiw&rel=0&showinfo=0" class="swipebox-video" rel="vB0orNAvUiw"></a>
-
-        <div class="video-box-text">
-          <h4>Historical Accurate Portfolio Part 2</h4>
-          8:07
-        </div>
-      </div>
-
-      <div class="video-box">
-        <a href="https://www.youtube.com/watch?v=Lfqb0TqQzNg&rel=0&showinfo=0" class="swipebox-video" rel="Lfqb0TqQzNg"></a>
-
-        <div class="video-box-text">
-          <h4>Historical Accurate Portfolio Part 3</h4>
-          7:53
-        </div>
-      </div>
-    </section>
   </article>
 </div>
 
