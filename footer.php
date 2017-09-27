f2cba74be1434154b98cfc4c7e617b5fa24f5e90
foresitegroup <mark@foresitegrp.com>
2017-09-27 15:54:23 -0500

    Make newsletter form actually do something

--- footer.php
+++ footer.php
@@ -33,15 +33,20 @@
             Stay up to date on investment news &amp; opportunities.<br>
             <br>
 
-            <form>
-              <div>
-                <label for="emailaddress">Email Address</label>
-                <input type="text" name="email" id="emailaddress" placeholder="EMAIL ADDRESS"><br>
-                <br>
-
-                <input type="submit" name="submit" value="SUBMIT">
+            <!-- Begin MailChimp Signup Form -->
+            <form action="//investmentaccountmanager.us16.list-manage.com/subscribe/post?u=b22e138d7cc44a38df49c7f7b&amp;id=2d370c2917" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <div class="mc-field-group">
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b22e138d7cc44a38df49c7f7b_2d370c2917" tabindex="-1" value=""></div>
+                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="EMAIL ADDRESS"><br><br>
+                <input type="submit" value="SUBMIT" name="subscribe" id="mc-embedded-subscribe">
+              </div>
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
               </div>
             </form>
+            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+            <!--End mc_embed_signup-->
           </div>
 
           <div class="three-col">
