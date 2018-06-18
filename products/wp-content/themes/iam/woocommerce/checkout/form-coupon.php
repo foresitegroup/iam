6f927ccb687c525397a50c334faa04e2a483b304
foresitegroup <mark@foresitegrp.com>
2018-06-18 14:24:51 -0500

    WooCommerce updates

--- products/wp-content/themes/iam/woocommerce/checkout/form-coupon.php
+++ products/wp-content/themes/iam/woocommerce/checkout/form-coupon.php
@@ -4,7 +4,7 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @version     3.4.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
--- products/wp-content/themes/iam/woocommerce/emails/email-order-items.php
+++ products/wp-content/themes/iam/woocommerce/emails/email-order-items.php
@@ -4,7 +4,7 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates/Emails
- * @version   3.2.0
+ * @version   3.4.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
